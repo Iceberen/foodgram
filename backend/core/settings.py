110f53cbc1b8aa56cc790c187b95f5c6b2a2645d
Vyacheslav Vasilyev <vgv1tpu@yandex.ru>
2025-04-12 20:47:54 +0300

    fix

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -92,11 +92,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/backend_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/var/www/foodgram/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
